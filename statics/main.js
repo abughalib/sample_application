288c4bea773886295a0b246118ea5b94ad7785f6
abugh <abughalib64@gmail.com>
2022-06-18 12:36:53 +0530

    added router

--- statics/main.js
+++
@@ -1,9 +0,0 @@
-let nav = document.querySelector('#nav');
-
-window.onscroll = ()=>{
-	if(window.pageYOffset > 0){
-		nav.classList.add('navBar-scrolled')
-	}else{
-		nav.classList.remove('navBar-scrolled')
-	}
-}
