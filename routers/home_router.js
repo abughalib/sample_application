1f696b6bb94270fc18bee4c061e7a4ce591b551c
abugh <abughalib64@gmail.com>
2022-06-19 13:52:37 +0530

    chart added & nav fix

--- routers/home_router.js
+++ routers/home_router.js
@@ -1,14 +1,38 @@
 const express = require('express')
 const {requireAuth} = require("./auth_handler");
+const sqlite = require("sqlite3");
 
 let router = express.Router();
 
-router.get('', requireAuth, (req, res)=>{
-    let user = null;
-    if (req['session'].user) {
-        user = req['session'].user;
+function get_age_data(rows) {
+    let ages = []
+    for(data of rows){
+        ages.push(data['age']);
     }
-    res.render('index', {"user": user})
+    return ages;
+}
+
+function get_salary_data(rows) {
+    let salary = [];
+    for (data of rows){
+        salary.push(data['salary']);
+    }
+    return salary;
+}
+
+router.get('', requireAuth, (req, res)=>{
+
+    let db = new sqlite.Database('database.sql', (err)=>{
+        if (err) throw err;
+    });
+    db.all(`SELECT * from Employees`, (err, rows)=>{
+        if (err) throw err;
+
+        let age_data = get_age_data(rows);
+        let salary_data = get_salary_data(rows);
+
+        return res.render('index.ejs', {'age_data': age_data, 'salary_data': salary_data});
+    });
 });
 
 router.all('*', (req, res)=>{
